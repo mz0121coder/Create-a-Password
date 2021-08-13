bcbef94162f37601fe65efe3871791d9b14e0fb2
Mohammad Zafar <sz7861@outlook.com>
2021-08-13 13:42:07 +0100

    Function for uppercase values + event listener

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ let checkLowerWanted;
 alphabetsList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 numberOptionsList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 otherChoicesList = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+let userSelection;
+
+let bigCharacters = function (lettersInput) {
+  return lettersInput.toUpperCase();
+};
+convertToUpper = alphabetsList.map(bigCharacters);
+
+let generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", function () {
+  joinAndCreate = makeUniquePassword();
+  document.getElementById("password").placeholder = joinAndCreate;
+});
+
