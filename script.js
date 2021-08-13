4fd086f0109d37b0da23b6c6dc4da98743f3b110
Mohammad Zafar <sz7861@outlook.com>
2021-08-13 13:41:01 +0100

    Create array for user choices

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ let checkNumbersWanted;
 let checkOtherChoices;
 let checkUpperWanted;
 let checkLowerWanted;
+// Arrays for user choices
+alphabetsList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+numberOptionsList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+otherChoicesList = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
